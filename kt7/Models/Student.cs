99be3039fe9bd5495032f83306d8b88af576fa6c
Hotarru <128384346+Hotarru@users.noreply.github.com>
2024-11-02 15:50:15 +1000

    Delete kt7 directory

--- kt7/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace kt7.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-}
